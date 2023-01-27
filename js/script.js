38e9d1cc4c3d0d5a2c4277f4867e302f0233187b
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-27 19:04:07 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            apiAddr: './server.php',
+            discList: [],
+            activeCard: 0
+        }
+    },
+    methods: {
+        getData() {
+            axios.get(this.apiAddr)
+                .then((response) => {
+                    console.log(response.data);
+                    this.discList = response.data;
+                })
+                .catch((error) => {
+                    console.error(error);
+                })
+        },
+    },
+    created() {
+        this.getData();
+    },
+}).mount('#app');
